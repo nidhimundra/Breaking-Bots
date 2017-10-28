35ca8d493e8e05eec3ca1f003e93aa4b2f8e7f17
Nidhi Mundra <nidhi.mundra05@gmail.com>
2017-10-28 12:16:18 -0400

    Add configuration support to the crawler

--- crawler.py
+++ crawler.py
@@ -1,15 +1,33 @@
 import scrapy
 import csv
 
+# Change csv_filename to fetch data from some other CSV file
+csv_filename = "top-1000"
+
+# Change config to fetch configuration from some other CSV file
+from config import *
 
 class BreakingBotsSpider(scrapy.Spider):
     name = "breaking-bots"
-    with open("top-1000.csv", "rb") as f:
+
+    with open(csv_filename + ".csv", "rb") as f:
         urls = []
+
         reader = csv.reader(f)
         for i, line in enumerate(reader):
             urls.append("http://www." + line[1])
         start_urls = urls
+
+    def start_requests(self):
+        for u in self.start_urls:
+            yield scrapy.Request(u, 
+                method=METHOD,
+                headers=HEADERS,
+                body=BODY,
+                cookies=COOKIES,
+                meta=META,
+                encoding=ENCODING,
+                dont_filter=DONT_FILTER)
 
     def parse(self, response):
         yield {
