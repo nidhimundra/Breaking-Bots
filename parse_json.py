c9af33928c5a2ef455fb71e12da0294f25247647
Nidhi Mundra <nidhi.mundra05@gmail.com>
2017-11-08 18:37:23 -0500

    Add script file to parse json output and create csv

---
+++ parse_json.py
@@ -0,0 +1,27 @@
+import json
+import csv
+
+file_name = "settings1"
+json_file = open(file_name + ".json","r")
+objects = json.load(json_file)
+
+output_file = open(file_name + ".csv", "wb")
+output_writer = csv.writer(output_file)
+
+output_writer.writerow(["Website", "Status", "Error", "Message"])
+
+for obj in objects:
+	row = [obj["url"], "", "", ""]
+	if "status" in obj.keys():
+		row[1] = obj["status"]
+	if "type" in obj.keys():
+		row[2] = obj["type"]
+	if "reason" in obj.keys():
+		row[3] = obj["reason"]
+	output_writer.writerow(row)
+
+output_file.close()
+
+
+
+
