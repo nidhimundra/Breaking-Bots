91a7bdc27b6330acabf51c2c30be9931b2dae981
Akanksha Sharma <akankshasharma033@gmail.com>
2017-11-09 16:39:52 -0500

    proxy commit

--- scrapy_settings.py
+++ scrapy_settings.py
@@ -1,2 +1,12 @@
 CUSTOM_SETTINGS = {
+    "RETRY_TIMES" : 10,
+    "RETRY_HTTP_CODES" : [500, 503, 504, 400, 403, 404, 408],
+    "DOWNLOADER_MIDDLEWARES" : {
+        'scrapy.downloadermiddlewares.retry.RetryMiddleware': 90,
+        'scrapy_proxies.RandomProxy': 100,
+        'scrapy.downloadermiddlewares.httpproxy.HttpProxyMiddleware': 110,
+    },
+    "PROXY_LIST" : '/path/to/proxy/list.txt',
+    "PROXY_MODE" : 0
+
 }
