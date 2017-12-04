d5d44a4fccd322677533b2278e85864706e6de7a
Nidhi Mundra <nidhi.mundra05@gmail.com>
2017-12-03 22:52:23 -0500

    Add top-500-urls

--- scrapy_settings.py
+++ scrapy_settings.py
@@ -1,3 +1,6 @@
-NAME = "Referer"
+NAME = "user-agent"
 CUSTOM_SETTINGS = {
+    # "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36"
+    "USER_AGENT" : "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36"
+    #Linux :  "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36"
 }
